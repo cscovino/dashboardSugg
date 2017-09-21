5022f1f35f50258d2f691766b8a19f5311056702
cscovino <kikescovino@gmail.com>
2017-09-20 22:58:27 -0400

    update

--- suggestions/public/js/index.js
+++ suggestions/public/js/index.js
@@ -12,10 +12,33 @@ var app = {
 
   	setSnap: function(snap){
   		app.model = snap;
-  		app.refresh(app.model.pendientes,app.model.completadas);
+  		app.refreshChart(Object.keys(app.model.pendientes).length,Object.keys(app.model.completadas).length);
+  		app.refreshPends();
   	},
 
-	refresh: function(pend,comp){
+  	sendSuggestion: function(){
+  		document.getElementById('detalles').style.display = 'none';
+  		var sugg = document.getElementById('suggestion').value;
+  		firebase.database().ref('pendientes').push({suggestion:sugg,percent:0});
+  	},
+
+  	refreshPends: function(){
+  		var suggs = $('#pends');
+		suggs.html('');
+		var codigo = '<ul>';
+  		for(var key in app.model.pendientes){
+  			codigo += '<li>'+app.model.pendientes[key].suggestion+'</li>';
+  			//codigo += '<div class="progress" style="width: 50%;"><div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"' 
+  			codigo += "<input id='ex8' data-slider-id='ex1Slider' type='text' data-slider-min='0' data-slider-max='100' data-slider-step='1' data-slider-value='45'/>";
+  			//codigo += 'style="width:'+app.model.pendientes[key].percent+'%"></div></div>';
+  		}
+  		codigo += '</ul>';
+  		suggs.append(codigo);
+  		$("#ex8").slider();
+  		console.log($("#ex8").slider().getValue());
+  	},
+
+	refreshChart: function(pend,comp){
 		var ctx = $("#pieChart").get(0).getContext("2d");
 		var data = {
 		    datasets: [{
